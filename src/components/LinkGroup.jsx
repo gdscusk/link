315ed6598050133be2d1c184617daef92ca056e2
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-12-14 01:46:58 +0700

    Added JSON as links source | Update on head tag

--- src/components/LinkGroup.jsx
+++ src/components/LinkGroup.jsx
@@ -1,7 +1,7 @@
-export default function LinkGroup({ title, children }) {
+export default function LinkGroup({ groupName, children }) {
   return (
     <div className="flex flex-col p-4 rounded-xl my-2">
-      <h2 className="font-lg font-bold mb-2 dark:text-white">{title}</h2>
+      <h2 className="font-lg font-bold mb-2 dark:text-white">{groupName}</h2>
       {children}
     </div>
   );
